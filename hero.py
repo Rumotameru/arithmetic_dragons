a51b50e064560410b31344a3d4169e348abd41ba
Rumotameru <rumotameru@gmail.com>
2016-11-24 16:07:43 +0300

    Update hero.py

--- hero.py
+++ hero.py
@@ -8,3 +8,20 @@ from gameunit import *
 Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
 
 """
+class Hero(Attacker):
+    def __init__(self, name = 'Player1'):
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
+        self._name = name
+
+    def attack(self, target):
+        target._health -= self._attack
+        if target is Dragon():
+            self._experience += 50
+        else:
+            self._experience += 25
+
+    def gameOver(self):
+        if self._health <= 0:
+            print('К сожалению, Вы проиграли...')
